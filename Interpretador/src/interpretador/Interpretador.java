f968f7c858ed7e4f814a058eb665df4956ce95df
Felipemv <30699913+Felipemv@users.noreply.github.com>
2017-09-15 18:56:50 -0300

    Add files via upload
    Troca das classes FileReader e BufferedReader por RandomAccessFile
    Inclusão do Program Counter

--- Interpretador/src/interpretador/Interpretador.java
+++ Interpretador/src/interpretador/Interpretador.java
@@ -7,7 +7,6 @@ package interpretador;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -29,23 +28,24 @@ public class Interpretador{
     private static int address;
     private static String code;
     private static int[] memoria;
+    private static int pc;
     
     private static FileWriter fw;
     private static BufferedWriter bw;
     
     public static void main(String[] args) throws IOException {
-        FileReader fr = new FileReader("operacao.txt");
-        BufferedReader br = new BufferedReader(fr);
+        RandomAccessFile raf = new RandomAccessFile("operacao.txt", "r");
         
         memoria = new int[32];
+        pc = 0;
         
-        type(br);         
-        br.close();
-        bw.close();
+        type(raf);         
+        raf.close();
+        
     }
 
     //Identifica o tipo de codigo (R, I ou J)
-    private static void type(BufferedReader br) throws IOException{
+    private static void type(RandomAccessFile raf) throws IOException{
         /*
             5 primeiros bits correspondem ao tipo de codigo
             
@@ -54,24 +54,30 @@ public class Interpretador{
             Outros - Tipo I
         */
         
-        code = br.readLine();
-        code = code.replaceAll(" ", "");
-        String subCode = code.substring(0, 6);        
-        int value = binary(subCode, subCode.length());
-        
-        switch (value) {
-            case 0:
-                format = 'R';
-                break;
-            case 2:
-            case 3:
-                format = 'J';
-                break;
-            default:
-                format = 'I';
-                break;
-        }
-        breakCode();
+        raf.seek(0);
+        while((code = raf.readLine()) != null){            
+            pc += code.length()+2;
+            raf.seek(pc);
+
+            code = code.replaceAll(" ", "");
+            String subCode = code.substring(0, 6);        
+            int value = Integer.parseInt(subCode,2);
+
+            switch (value) {
+                case 0:
+                    format = 'R';
+                    break;
+                case 2:
+                case 3:
+                    format = 'J';
+                    break;
+                default:
+                    format = 'I';
+                    break;
+            }
+            breakCode();
+            
+        }       
     }
     
     //Separa o codigo em partes
@@ -84,63 +90,24 @@ public class Interpretador{
         
         switch(format){
             case 'R':
-                rs = binary(code.substring(6, 11), 5);
-                rt = binary(code.substring(11, 16), 5);
-                rd = binary(code.substring(16, 21), 5);
-                shamt = binary(code.substring(21, 26), 5);
-                funct = binary(code.substring(26, 32), 6);
+                rs = Integer.parseInt(code.substring(6, 11), 2);
+                rt = Integer.parseInt(code.substring(11, 16), 2);
+                rd = Integer.parseInt(code.substring(16, 21), 2);
+                shamt = Integer.parseInt(code.substring(21, 26), 2);
+                funct = Integer.parseInt(code.substring(26, 32), 2);
                 executeTypeR();
                 break;
             
             case 'J':
-                address = binary(code.substring(6, 32), 26);
+                address = Integer.parseInt(code.substring(6, 32), 2);
                 break;
             
             default:
-                rs = binary(code.substring(6, 11), 5);
-                rt = binary(code.substring(11, 16), 5);
-                imm = binary(code.substring(16, 32), 16);
+                rs = Integer.parseInt(code.substring(6, 11), 2);
+                rt = Integer.parseInt(code.substring(11, 16), 2);
+                imm = Integer.parseInt(code.substring(16, 32), 2);
                 break;
         }
-    }
-    
-    private static int loadWord(int registro){
-        
-        registro = memoria[registro];
-        return registro;
-    }
-    
-    private static void storeWord (int registro, int posMemory, int valor) throws IOException{
-        //escreve no txt de memoria o registro
-       memoria[registro] = valor;
-       fw = new FileWriter("saida.txt", false);
-       bw = new BufferedWriter(fw);
-       
-        for (int i = 0; i < memoria.length; i++) {
-            bw.write(Integer.toBinaryString(memoria[i])+"\n");
-        }
-                
-    }
-    
-    private static void add(int rd, int rs, int rt) throws IOException{
-        //rd = rs + rt;
-        rs = loadWord(rs);
-        rt = loadWord(rt);
-        rd = rs + rt;
-        storeWord(rt, 0, rd);
-    }
-    
-    //Converte uma string(em binario) em um valor inteiro
-    private static int binary(String subCode, int size) {
-        int value = 0;
-        int n = (int) Math.pow(2, (size-1));
-        for (int i = 0; i < size; i++) {
-            if(subCode.charAt(i) == '1'){
-                value += n;
-            }
-            n /= 2;
-        }
-        return value;
     }
     
     private static void executeTypeR() throws IOException{
@@ -154,4 +121,43 @@ public class Interpretador{
                 break;
         }
     }
+    
+    private static void executeTypeI() throws IOException{
+        switch(funct)
+        {
+            case 32:
+                add(rd, rs, rt);
+                break;
+            
+            default:
+                System.out.println("Invalid Operation");
+                break;
+        }
+    }
+    
+    private static int loadWord(int registro){
+        
+        registro = memoria[registro];
+        return registro;
+    }
+    
+    private static void storeWord (int registro, int valor) throws IOException{
+        //escreve no txt de memoria o registro
+       memoria[registro] = valor;
+       fw = new FileWriter("saida.txt", false);
+       bw = new BufferedWriter(fw);
+       
+        for (int i = 0; i < memoria.length; i++) {
+            bw.write(Integer.toBinaryString(memoria[i])+"\n");
+        }
+        bw.close();
+    }
+    
+    private static void add(int rd, int rs, int rt) throws IOException{
+        //rd = rs + rt;
+        rs = loadWord(rs);
+        rt = loadWord(rt);
+        rd = rs + rt;
+    }
+    
 }
