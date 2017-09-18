d7a089f11777ff4c2d6485b6cc9a74b10a2c859e
Felipemv <30699913+Felipemv@users.noreply.github.com>
2017-09-18 10:39:17 -0300

    Add files via upload
    Correção de erros no método storeWord().
    Controle de estouro de memória implementado.

--- Interpretador/src/interpretador/Interpretador.java
+++ Interpretador/src/interpretador/Interpretador.java
@@ -9,19 +9,20 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Home
  */
-public class Interpretador{
+public class Interpretador {
 
     //Registradores
     private static int rs;
     private static int rt;
     private static int rd;
     private static int pc;
-    
+
     private static char format;
     private static int opcode;
     private static int shamt;
@@ -29,56 +30,56 @@ public class Interpretador{
     private static int imm;
     private static int address;
     private static int[] general_register;
-        
+
     //Escritores
     private static FileWriter fw;
     private static BufferedWriter bw;
-    
+
     //Leitor de memoria
     private static final int BITSDADOS = 8;
     private static RandomAccessFile mem;
     private static int[] memoria;
-    
-    public static void main(String[] args) throws IOException {        
+
+    public static void main(String[] args) throws IOException {
         RandomAccessFile raf = new RandomAccessFile("operacao.txt", "r");
-        
+
         general_register = new int[32];
         for (int i = 0; i < general_register.length; i++) {
             general_register[i] = 0;
         }
         memoria = new int[32];
         mem = new RandomAccessFile("memoria.txt", "r");
-        
+
         pc = 0;
-        	
+
         for (int i = 0; i < memoria.length; i++) {
             memoria[i] = Integer.parseInt(mem.readLine(), 2);
         }
         mem.close();
-        
-        type(raf);         
+
+        type(raf);
         raf.close();
     }
 
     //Identifica o tipo de codigo (R, I ou J)
-    private static void type(RandomAccessFile raf) throws IOException{
+    private static void type(RandomAccessFile raf) throws IOException {
         /*
             6 primeiros bits correspondem ao tipo de codigo
             
             0 - Tipo R
             2 ou 3 - Tipo J
             Outros - Tipo I
-        */
+         */
         String code;
-        
+
         raf.seek(0);
-        while((code = raf.readLine()) != null){            
-            pc += code.length()+2;
+        while ((code = raf.readLine()) != null) {
+            pc += code.length() + 2;
             raf.seek(pc);
 
             code = code.replaceAll(" ", "");
-            String subCode = code.substring(0, 6);        
-            opcode = Integer.parseInt(subCode,2);
+            String subCode = code.substring(0, 6);
+            opcode = Integer.parseInt(subCode, 2);
 
             switch (opcode) {
                 case 0:
@@ -92,19 +93,19 @@ public class Interpretador{
                     format = 'I';
                     break;
             }
-            breakCode(code);            
-        }       
+            breakCode(code);
+        }
     }
-    
+
     //Separa o codigo em partes de acordo com o tipo de instrução
-    private static void breakCode(String code) throws IOException{
+    private static void breakCode(String code) throws IOException {
         /*
             R - opcode(6) | rs(5) | rt(5) | rd(5) | shamt(5) | funct(6)
             I - opcode(6) | rs(5) | rt(5) | imm(16)
             J - opcode(6) | address(26)
-        */
-        
-        switch(format){
+         */
+
+        switch (format) {
             case 'R':
                 rs = Integer.parseInt(code.substring(6, 11), 2);
                 rt = Integer.parseInt(code.substring(11, 16), 2);
@@ -113,11 +114,11 @@ public class Interpretador{
                 funct = Integer.parseInt(code.substring(26, 32), 2);
                 executeTypeR();
                 break;
-            
+
             case 'J':
                 address = Integer.parseInt(code.substring(6, 32), 2);
                 break;
-            
+
             default:
                 rs = Integer.parseInt(code.substring(6, 11), 2);
                 rt = Integer.parseInt(code.substring(11, 16), 2);
@@ -126,11 +127,10 @@ public class Interpretador{
                 break;
         }
     }
-    
+
     //Executa instruções do tipo R
-    private static void executeTypeR() throws IOException{
-        switch(funct)
-        {
+    private static void executeTypeR() throws IOException {
+        switch (funct) {
             case 32:
                 add(rd, rs, rt);
                 break;
@@ -139,62 +139,66 @@ public class Interpretador{
                 break;
         }
     }
-    
+
     //Executa instruções do tipo I
-    private static void executeTypeI() throws IOException{
-        switch(opcode)
-        {
+    private static void executeTypeI() throws IOException {
+        switch (opcode) {
             case 35:
                 general_register[rt] = loadWord(rs);
                 break;
             case 43:
-                storeWord(rs, general_register[rt]);
+                storeWord(rs, rd);
                 break;
             default:
                 System.out.println("Invalid Operation");
                 break;
         }
     }
-    
-    private static int loadWord(int registro) throws IOException{
-        mem = new RandomAccessFile("memoria.txt", "r");   
-        
-        mem.seek((BITSDADOS + 2)*registro);
-        registro = Integer.parseInt(mem.readLine(), 2);  
-        
+
+    private static int loadWord(int registro) throws IOException {
+        mem = new RandomAccessFile("memoria.txt", "r");
+
+        mem.seek((BITSDADOS + 2) * registro);
+        registro = Integer.parseInt(mem.readLine(), 2);
+
         mem.close();
         return registro;
     }
-    
-//escreve no txt de memoria o registro
-    private static void storeWord (int registro, int valor) throws IOException{        
-          memoria[registro] = valor;
-        
-        fw = new FileWriter("memoria.txt", false);
-        bw = new BufferedWriter(fw);
 
-        String info;
-       
-        for (int i = 0; i < memoria.length; i++) {
-            info = Integer.toBinaryString(memoria[i]);
-            
-            //Correção para que a string tenha a quantidade definida de bits
-            if(info.length() < BITSDADOS){
-                for (int j = info.length(); j < BITSDADOS; j++) {
-                    info = "0" + info;
+//escreve no txt de memoria o registro
+    private static void storeWord(int registro, int valor) throws IOException {
+        if (valor > Math.pow(2, BITSDADOS) - 1) {
+            System.out.println("Tamanho do espaço da memória excedido!!!");
+            JOptionPane.showMessageDialog(null, "Tamanho da posição de memória excedido!!!");
+        } else {
+            memoria[registro] = valor;
+
+            fw = new FileWriter("memoria.txt", false);
+            bw = new BufferedWriter(fw);
+
+            String info;
+
+            for (int i = 0; i < memoria.length; i++) {
+                info = Integer.toBinaryString(memoria[i]);
+
+                //Correção para que a string tenha a quantidade definida de bits
+                if (info.length() < BITSDADOS) {
+                    for (int j = info.length(); j < BITSDADOS; j++) {
+                        info = "0" + info;
+                    }
                 }
-            } 
-            bw.write(info);
-            bw.newLine();
+                bw.write(info);
+                bw.newLine();
+            }
+            bw.close();
         }
-        bw.close();
+
     }
-    
-    private static void add(int rd, int rs, int rt) throws IOException{
+
+    private static void add(int rd, int rs, int rt) throws IOException {
         //rd = rs + rt;
         rs = general_register[rs];
         rt = general_register[rt];
-        rd = rs + rt;
+        Interpretador.rd = rs + rt;
     }
-    
 }
